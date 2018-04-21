cdbc4f52329d52240501d16615d837117698bf6c
Carson Fox <carson.fox@aggiemail.usu.edu>
2018-04-21 16:47:42 -0600

    Some 1.3.3 exercises

--- chapter-1.3.3.scm
+++ chapter-1.3.3.scm
@@ -20,3 +20,35 @@
            (search-zeroes f b a 0.001))
           (else 
             (error "Values must be of opposite sign:" a b)))))
+
+(define (fixed-point f guess)
+  (define tolerance 0.0001)
+  (define (good-enough? a b)
+    (< (abs (- a b)) tolerance))
+  (let ((y (f guess)))
+    (if (good-enough? guess y)
+      guess
+      (fixed-point f y))))
+
+(define (square-root x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+;Let a = x, b = 1. The fixed point of x -> 1 + 1/x = (x + 1) / x. Substitution yields
+;a = (a + b) / a. Since b = 1, a = a / b. This is now the golden ratio: a / b = (a + b) / a
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+
+(define (fixed-point-display f guess n)
+  (define tolerance 0.0001)
+  (define (good-enough? a b)
+    (< (abs (- a b)) tolerance))
+  (let ((y (average guess (f guess))))
+    (display "Approximation number ")
+    (display n)
+    (display ": ")
+    (display y)
+    (newline)
+    (if (good-enough? guess y)
+      guess
+      (fixed-point-display f y (inc n)))))
+
